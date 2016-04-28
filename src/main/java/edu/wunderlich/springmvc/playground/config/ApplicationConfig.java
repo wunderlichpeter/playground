b7ed7510dd12409bcad8696b6569d8ddbfec115e
Peter Wunderlich <Peter Wunderlich>
2016-04-28 10:02:23 +0200

    Wir müssen uns nochmal mit der Einbindung von MySQL beschäftigen. Auf dem Stand funktioniert es jetzt erstmal

--- src/main/java/edu/wunderlich/springmvc/playground/config/ApplicationConfig.java
+++ src/main/java/edu/wunderlich/springmvc/playground/config/ApplicationConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import edu.wunderlich.springmvc.playground.Application;
 
 @Configuration
-@PropertySource("classpath:persistence.properties")
+@PropertySource("classpath:persistence-hsql.properties")
 @PropertySource("classpath:application.properties")
 @ComponentScan(basePackageClasses = Application.class)
 class ApplicationConfig {
